393c27e5b73e7329a114c19ef9b5a5425f4d9271
KassPerez <Kassandra.perez@utexas.edu>
2016-07-18 16:24:28 -0500

    Starter files

---
+++ assignment4_part_a/Header.java
@@ -0,0 +1,10 @@
+/* CRITTERS <MyClass.java>
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Kassandra Perez
+ * KAP2589
+ * <Student2 Name>
+ * <Student2 EID>
+ * Slip days used: <0>
+ * Summer 2016
+ */
---
+++ assignment4_part_a/Main.java
@@ -0,0 +1,20 @@
+/* CRITTERS Main.java
+ * EE422C Project 4 submission by
+ * Replace <...> with your actual data.
+ * Kassandra Perez
+ * kap2589
+ * <Student2 Name>
+ * <Student2 EID>
+ * <Student2 5-digit Unique No.>
+ * Slip days used: <0>
+ * Summer 2016
+ */
+package project4;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("GLHF");
+	}
+}
